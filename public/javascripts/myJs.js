4af90ac4bdc8030be40b6605c9019d1d5770de26
Ciro-Lopes <ciro.flopes@gmail.com>
2020-05-27 04:08:33 -0300

    player de musica inserido

--- public/javascripts/myJs.js
+++ public/javascripts/myJs.js
@@ -1,3 +1,60 @@
+//Player de Musica
+var volControl = document.getElementById("volControl");
+var song = new Audio();
+song.volume = 0.5;
+
+var musicas = ["", "./sounds/rock.mp3", "./sounds/celta.mp3", "/sounds/classica.mp3", "/sounds/chuva.mp3", "/sounds/riacho.mp3"];
+
+//alterar o dropdown
+function trocarDrop(id) {
+    var dropdown = document.getElementById("dropdownMenuLink");
+    for (var i = 0; i < 6; i++) {
+        if (id == i) {
+            document.getElementById(id).className = "dropdown-item active";
+            dropdown.innerHTML = document.getElementById(id).value;
+            dropdown.value = id;
+            playMusic(id);
+        }
+        else {
+            document.getElementById(i).className = "dropdown-item";
+        }
+    }
+}
+
+//Tocar a musica
+function playMusic(id) {
+    for (var i = 0; i < 6; i++) {
+        if (id == i) {
+            if (id == 0)
+                document.getElementById("playPouse").className = "fas fa-play";
+            else {
+                document.getElementById("playPouse").className = "fas fa-pause";
+            }
+            song.src = musicas[i];
+            song.play();
+        }
+    }
+}
+
+//Pausar e continuar musica
+function pausePLay() {
+    var playPause = document.getElementById("playPouse");
+    if (song.paused) {
+        song.play();
+        playPause.className = "fas fa-pause";
+    }
+    else {
+        song.pause();
+        playPause.className = "fas fa-play";
+    }
+}
+
+//Mudar volume
+function changeVolume(valor) {
+    song.volume = valor / 100;
+}
+
+//Botao toggle do responsivo
 function toggleButton() {
     var infoChat = document.getElementById("infoChat");
     var chat = document.getElementById("chatToggle");
